85b2c3f68bf6bb525047a868159a38c18aaf05a4
Jonathan Egert <jonegert@MacBook-Air-von-Jonathan.local>
2024-11-27 17:16:08 +0200

    hw4

--- Primes.java
+++ Primes.java
@@ -1,5 +1,21 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+       int n=Integer.parseInt(args[0]);
+      boolean[] numbers=new boolean[n+1];
+      for(int i=2;i<n+1;i++){
+        numbers[i]=true;
+      }
+      int i=2;
+      while(i<Math.sqrt(n)){
+        for(int j=2;j<n/i;j++){
+            numbers[i*j]=false;
+      }
+      i++;
+    }
+    for(int k=2;k<n;k++){
+        if(numbers[k]==true){
+            System.out.println(k);
+        }
+    }
     }
 }
